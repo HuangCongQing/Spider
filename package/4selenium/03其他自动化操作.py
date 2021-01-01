03d42ca5dd58cfd61dabcd702a5ca893a2d1d750
HuangCongQing <1756260160@qq.com>
2021-01-01 15:50:17 +0800

    add: 其他自动化操作

--- package/4selenium/03其他自动化操作.py
+++ package/4selenium/03其他自动化操作.py
@@ -4,7 +4,7 @@ Author: HCQ
 Company(School): UCAS
 Email: 1756260160@qq.com
 Date: 2021-01-01 15:15:27
-LastEditTime: 2021-01-01 15:18:17
+LastEditTime: 2021-01-01 15:49:58
 FilePath: /Spider/package/4selenium/03其他自动化操作.py
 '''
 from selenium import webdriver
@@ -13,4 +13,31 @@ bro = webdriver.Chrome(executable_path='./chromedriver')
 
 bro.get('https://www.taobao.com/')
 
-bro.find_element_by_id('q')
+
+#标签定位
+search_input = bro.find_element_by_id('q')
+#标签交互
+search_input.send_keys('Iphone')
+
+
+#执行一组js程序
+bro.execute_script(r'window.scrollTo(0,document.body.scrollHeight)') # 滚动一屏的高度
+sleep(2)
+#点击搜索按钮
+btn = bro.find_element_by_css_selector('.btn-search')
+btn.click()
+
+# ========================进入百度页面
+
+bro.get('https://www.baidu.com')
+sleep(2) # 等待2s
+#回退
+bro.back() # 点击返回按钮
+sleep(2)
+#前进
+bro.forward()
+
+sleep(5)
+
+bro.quit()
+
