3213c5f2785cd83935e8977197dc9342545e0a5c
HuangCongQing <1756260160@qq.com>
2021-01-04 10:35:24 +0800

    update

--- package/3xpath/05xpath解析基础.py
+++ package/3xpath/05xpath解析基础.py
@@ -4,7 +4,7 @@ Author: HCQ
 Company(School): UCAS
 Email: 1756260160@qq.com
 Date: 2021-01-01 13:56:16
-LastEditTime: 2021-01-04 10:28:13
+LastEditTime: 2021-01-04 10:34:44
 FilePath: /Spider/package/3xpath/05xpath解析基础.py
 '''
 #!/usr/bin/env python 
@@ -12,7 +12,7 @@ FilePath: /Spider/package/3xpath/05xpath解析基础.py
 from lxml import etree
 if __name__ == "__main__":
     #实例化好了一个etree对象，且将被解析的源码加载到了该对象中
-    tree = etree.parse('test.html')
+    tree = etree.parse('test.html')  # 在线网页 用 ：  etree.HTML(page_text)
     r = tree.xpath('/html/body/div')   #  3个Element对象 [<Element div at 0x7fcb3819b4c8>, <Element div at 0x7fcb3819b5c8>, <Element div at 0x7fcb3819b608>]
     # r = tree.xpath('/html//div') # 等价于上面/html/body/div
     # r = tree.xpath('//div')  #  # 等价于上面
