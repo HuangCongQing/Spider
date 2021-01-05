2959074ad6ee278bcb07fe987384289d8aec693e
HuangCongQing <1756260160@qq.com>
2021-01-05 12:42:02 +0800

    add: 视频爬取并存储

--- 10bilibili视频爬取下载/bilibili视频标题和链接爬取.py
+++ 10bilibili视频爬取下载/bilibili视频标题和链接爬取.py
@@ -4,13 +4,14 @@ Author: HCQ
 Company(School): UCAS
 Email: 1756260160@qq.com
 Date: 2021-01-03 14:57:10
-LastEditTime: 2021-01-05 10:22:32
+LastEditTime: 2021-01-05 12:41:31
 FilePath: /Spider/10bilibili视频爬取下载/bilibili视频标题和链接爬取.py
 '''
 import requests
 import re
 from bs4 import BeautifulSoup
 import openpyxl
+import xlwt			# 存储excel
 
 def read_excel():
     #定义一个空列表
@@ -28,7 +29,8 @@ def read_excel():
 def get_details(schools):
     url_all = []
     title_all = []
-    for school in schools[0:21]:
+    no_info = 0
+    for school in schools:
         # print(school) # 北京大学
         word = school +  " 宣传片 最新"  	# 解析，用于组成URL
         # print(word)
@@ -51,20 +53,45 @@ def get_details(schools):
             # 封面为空
             # print(soup.select('.lazy-img > img')[0]) # <img alt="" src=""/>??????
             # img_link = 'https:' +  soup.select('.lazy-img > img')[0]['src']
-            print(url_link)
-            print(title)
+            # print(url_link)
+            print( title,  "  已爬取")
             url_all.append(url_link)
             title_all.append(title)
         else:
+            no_info = no_info + 1
             url_all.append('')
             title_all.append('')
-    # print(title_all)
-    return url_all,  title_all
+            print(school, " 没有相关视频，未爬取")
+    print("没有获得相关学校视频链接总数：", no_info)
+    return schools, url_all,  title_all
+
+
+def write_excel(schools_all, url_all,  title_all ):
+	#创建工作簿
+	workbook = xlwt.Workbook(encoding='utf-8')  
+	#创建sheet
+	data_sheet = workbook.add_sheet('bilibili爬取')  
+	# 测试数据
+	col0 = schools_all
+	col1 = url_all
+	col2 = title_all
+	
+	#生成第一列和第二列
+	for i in range(len(col0)):
+		data_sheet.write(i, 0, col0[i]) # set_style 设置属性
+		data_sheet.write(i, 1, col1[i])
+		data_sheet.write(i, 2, col2[i])
+	
+	#保存文件
+	workbook.save('bilibili视频标题和链接爬取.xls')	
+	
 
 if __name__ == "__main__":
     college = read_excel()
     # print(college[0])
-    url_all,  title_all = get_details(college)
-    print(url_all, title_all)
+    schools_all, url_all,  title_all = get_details(college)
+    # print(url_all, title_all)
+    write_excel(schools_all, url_all,  title_all )
+    print("爬取完毕，共爬取",len(schools_all), "学校" )
     
     
