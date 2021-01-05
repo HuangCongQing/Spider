d8923392b68edfa52f94ab204c68370292ccb3fa
HuangCongQing <1756260160@qq.com>
2021-01-05 11:16:00 +0800

    add: 读取xls

--- 文件操作/excel/01xlrd读取.py
+++ 文件操作/excel/01xlrd读取.py
@@ -1,11 +1,34 @@
 '''
-Description: 读取excel
+Description: 读取excel  https://blog.csdn.net/weixin_30383279/article/details/96566508?
 Author: HCQ
 Company(School): UCAS
 Email: 1756260160@qq.com
 Date: 2021-01-04 21:19:39
-LastEditTime: 2021-01-05 10:33:52
+LastEditTime: 2021-01-05 11:12:31
 FilePath: /Spider/文件操作/excel/01xlrd读取.py
 '''
 
 
+# _*_ coding:utf-8 _*_
+ 
+#----------------------------------------------------------------------------
+# import modules 
+#----------------------------------------------------------------------------
+import os
+import xlrd
+from datetime import date,datetime
+ 
+#打开Excel文件
+workbook = xlrd.open_workbook('/home/hcq/python/Spider/文件操作/excel/附件3：学生信息汇总.xls')
+ 
+#输出Excel文件中所有sheet的名字
+print(workbook.sheet_names()) # ['Sheet1', 'Sheet2', 'Sheet3']
+ 
+# 获取一个工作表
+table = workbook.sheets()[0]          #通过索引顺序获取
+# table = workbook.sheet_by_index(0) #通过索引顺序获取
+# table = workbook.sheet_by_name(u'Sheet1')#通过名称获取
+
+# 获取整行和整列的值（数组）　　
+print(table.row_values(0)) # 行
+print(table.col_values(1)[1:])  # 列（去除第一个）  ['黄深度', '是大V辅导费', 'DVD发', '电饭煲']
--- 文件操作/excel/02xlwt存储.py
+++ 文件操作/excel/02xlwt存储.py
@@ -4,6 +4,8 @@ Author: HCQ
 Company(School): UCAS
 Email: 1756260160@qq.com
 Date: 2021-01-04 21:41:17
-LastEditTime: 2021-01-04 21:41:17
-FilePath: /Spider/文件操作/excel/02存储excel.py
+LastEditTime: 2021-01-05 10:35:24
+FilePath: /Spider/文件操作/excel/02xlwt存储.py
 '''
+
+
