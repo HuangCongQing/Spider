c008bff74c4afbf371154b6a7272eae7a8536ba3
HuangCongQing <1756260160@qq.com>
2021-01-08 23:40:45 +0800

    add: 若有返回值一定是个列表

--- 多线程异步/02线程池基本使用.py
+++ 多线程异步/02线程池基本使用.py
@@ -4,7 +4,7 @@ Author: HCQ
 Company(School): UCAS
 Email: 1756260160@qq.com
 Date: 2021-01-01 13:56:16
-LastEditTime: 2021-01-08 23:38:42
+LastEditTime: 2021-01-08 23:40:29
 FilePath: /Spider/多线程异步/02线程池基本使用.py
 '''
 # import time
@@ -40,7 +40,7 @@ name_list =['xiaozi','aa','bb','cc']
 #实例化一个线程池对象
 pool = Pool(4)
 #将列表中每一个列表元素传递给get_page进行处理。
-pool.map(get_page,name_list)  # 函数 ，参数
+pool.map(get_page,name_list)  # 函数 ，参数,   若有返回值一定是个列表
 pool.close()
 pool.join()
 end_time = time.time()
