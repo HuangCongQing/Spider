e5e84428dc88b49b9044f5cd32e02dee793ed34d
HuangCongQing <1756260160@qq.com>
2021-01-16 20:45:54 +0800

    csv文件

---
+++ 文件操作/csv/01读取csv文件.py
@@ -0,0 +1,9 @@
+'''
+Description: 
+Author: HCQ
+Company(School): UCAS
+Email: 1756260160@qq.com
+Date: 2021-01-16 20:45:18
+LastEditTime: 2021-01-16 20:45:18
+FilePath: /Spider/文件操作/csv/01读取csv文件.py
+'''
---
+++ 文件操作/csv/02保存为csv文件.py
@@ -0,0 +1,9 @@
+'''
+Description: 保存为csv文件
+Author: HCQ
+Company(School): UCAS
+Email: 1756260160@qq.com
+Date: 2021-01-16 20:45:34
+LastEditTime: 2021-01-16 20:45:46
+FilePath: /Spider/文件操作/csv/02保存为csv文件.py
+'''
