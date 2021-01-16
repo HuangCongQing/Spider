9c3c075e66483975044c6b31482171deb7f6a8be
HuangCongQing <1756260160@qq.com>
2021-01-16 20:35:06 +0800

    add: jieba

--- package/6wordcloud&jieba/01jieba库使用.py
+++ package/6wordcloud&jieba/01jieba库使用.py
@@ -4,6 +4,14 @@ Author: HCQ
 Company(School): UCAS
 Email: 1756260160@qq.com
 Date: 2021-01-16 17:43:40
-LastEditTime: 2021-01-16 17:43:40
-FilePath: /Spider/package/6wordcloud/01jieba库使用.py
+LastEditTime: 2021-01-16 20:34:49
+FilePath: /Spider/package/6wordcloud&jieba/01jieba库使用.py
 '''
+import jieba
+ 
+# text = open("test.txt","rb").read()
+text = '发了困难看能否看上你分离开阿金发我我发往就鞥而过南人生观你是大V收代理费'
+#结巴分词
+wordlist = jieba.cut(text,cut_all=True)
+wl = " ".join(wordlist)   #  空格分词
+print(wl)#输出分词之后的txt
