87b19d1e62ad1fd42800a102a2915d5b09123f8b
HuangCongQing <1756260160@qq.com>
2021-03-01 00:25:13 +0800

    add: 不规则表格

--- practice/16中国教育在线/重点学科不规则表格.py
+++ practice/16中国教育在线/重点学科不规则表格.py
@@ -4,7 +4,7 @@ Author: HCQ
 Company(School): UCAS
 Email: 1756260160@qq.com
 Date: 2021-02-28 23:40:26
-LastEditTime: 2021-03-01 00:08:30
+LastEditTime: 2021-03-01 00:24:55
 FilePath: /Spider/practice/16中国教育在线/重点学科不规则表格.py
 '''
 import pandas as pd
@@ -20,5 +20,9 @@ url = 'http://www.cdgdc.edu.cn/xwyyjsjyxx/xwbl/zdjs/zdxk/zdxkmd/lsx/266612.shtml
 
 
 ''' 生成csv文件 '''
+print( pd.read_html(url)[0])
+print( pd.read_html(url)[0][0].values) # dataframe和np.array的相互转换
+print( pd.read_html(url)[0][0].values[1:-1]) # 去掉开头和结尾
 for table in pd.read_html(url):
+    # print(table)
     table.to_csv('重点学科不规则表格.csv', mode='a',encoding='utf-8',header=0,index=False)
