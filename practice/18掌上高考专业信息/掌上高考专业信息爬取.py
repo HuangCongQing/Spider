f96e37a43bc7e8b0f5189edce3c286b88f4d09fe
HuangCongQing <1756260160@qq.com>
2021-03-13 21:35:35 +0800

    update

---
+++ practice/18掌上高考专业信息/掌上高考专业信息爬取.py
@@ -0,0 +1,10 @@
+'''
+Description: 抓取掌上高考专业信息（除了开设院校，其余的信息全都要） https://gkcx.eol.cn/special
+Author: HCQ
+Company(School): UCAS
+Email: 1756260160@qq.com
+Date: 2021-03-13 21:13:44
+LastEditTime: 2021-03-13 21:13:54
+FilePath: /Spider/practice/18掌上高考专业信息/专业信息爬取.py
+'''
+
