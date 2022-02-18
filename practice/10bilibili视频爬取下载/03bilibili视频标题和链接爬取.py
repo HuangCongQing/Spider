457851a465512b65a09c00cc9f339a0035ed94bb
HuangCongQing <1756260160@qq.com>
2022-02-18 15:25:25 +0800

    add

--- practice/10bilibili视频爬取下载/03bilibili视频标题和链接爬取.py
+++ practice/10bilibili视频爬取下载/03bilibili视频标题和链接爬取.py
@@ -17,7 +17,7 @@ def read_excel():
     #定义一个空列表
     stu_num=[]
     #打开目标execl，这里注意openpyxl能读取的execl后缀名是'.xlsx'文件
-    workbook1=openpyxl.load_workbook(r'/home/hcq/python/Spider/文件操作/excel/college.xlsx')
+    workbook1=openpyxl.load_workbook(r'../../文件操作/excel/college.xlsx')
     #选定目标sheet
     worksheet1 = workbook1.active
     for cell in worksheet1['A']:
